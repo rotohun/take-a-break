b9468e239f6248c388d50345b722fc438a72257a
Ronald Hunter <ronaldtoddhunter@yahoo.com>
2017-10-07 13:18:51 -0400

    Updated wait time function

--- takeabreak.py
+++ takeabreak.py
@@ -1,3 +1,12 @@
+import time
 import webbrowser
 
-webbrowser.open("https://www.youtube.com/watch?v=vLdbJNcUIQs&t=37s")
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+while (break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    break_count = break_count + 1
+
